6cdcd875475b5b51286ba33d09224b4c33dc0450
Carlos Sejas <carlos.sejas@ucb.edu.bo>
2023-08-10 00:31:31 -0400

    primeros cambios

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,11 @@
+import multiplicar from "./multiplicador";
 import sumar from "./sumador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
+const mul =document.querySelector("#resul-mul");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -12,4 +14,5 @@ form.addEventListener("submit", (event) => {
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  mul.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
 });
